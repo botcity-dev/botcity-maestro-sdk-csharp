4f1628d5604f69c64dd2e2d7636566fddb45e93b
Edson Márcio <edson.marcio7@gmail.com>
2022-09-03 14:53:31 -0300

    testeIgnore

--- BotCity.TestsNUnit/ClassSenha.cs
+++ BotCity.TestsNUnit/ClassSenha.cs
@@ -8,7 +8,7 @@ namespace BotCity.TestsNUnit
 {
     public static class ClassSenha
     {
-        public static string Password = "boyFodase1!";
+        public static string Password = "!";
         public static string UserName = "Edson Márcio Oliveira"; 
     }
 }
